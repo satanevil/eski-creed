38389acb45dfd81aa011ab044b9d5918c7af6183
satanevil <satanevil150@gmail.com>
2016-02-19 01:20:00 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {144132967,139763200,107264463,97343360},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
